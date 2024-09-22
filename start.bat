f04fa4270aa466c32947484717e7a1ca1388db3f
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-22 19:16:58 +0900

    server update : 74から89に

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\Java\jdk-21\bin\java.exe" -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -Xms3G -Xmx3G -jar paper-1.21.1-74.jar nogui
+"C:\Program Files\Java\jdk-21\bin\java.exe" -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -Xms3G -Xmx3G -jar paper-1.21.1-89.jar nogui
 pause
